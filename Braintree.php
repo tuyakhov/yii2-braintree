59ec1e61a6c0960584a70842fb4c98965a11ed3b
Anton <atuyakhov@gmail.com>
2014-05-28 09:54:36 +0300

    fix commit

--- Braintree.php
+++ Braintree.php
@@ -1,7 +1,151 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: anton
- * Date: 27.05.14
- * Time: 15:42
- */ 
+ * @copyright Anton Tuyakhov <atuyakhov@gmail.com>
+ */
+namespace yii\braintree;
+
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+class Braintree extends Component
+{
+    private $environment = 'sandbox';
+    private $merchantId;
+    private $publicKey;
+    private $privateKey;
+
+    public $options;
+
+    /**
+     * Sets up Braintree configuration from config file
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function init()
+    {
+        foreach (['merchantId', 'publicKey', 'privateKey', 'environment'] as $attribute) {
+            if ($this->$attribute === null) {
+                throw new InvalidConfigException(strtr('"{class}::{attribute}" cannot be empty.', [
+                    '{class}' => static::className(),
+                    '{attribute}' => '$' . $attribute
+                ]));
+            }
+            \Braintree_Configuration::$attribute($this->$attribute);
+        }
+        parent::init();
+    }
+
+    /**
+     * Braintree sale function
+     */
+    public function singleCharge($submitForSettlement = true, $storeInVaultOnSuccess = true)
+    {
+        $this->options['options']['submitForSettlement'] = $submitForSettlement;
+        $this->options['options']['storeInVaultOnSuccess'] = $storeInVaultOnSuccess;
+        $result = \Braintree_Transaction::sale($this->options);
+
+        if ($result->success) {
+            return ['status' => true, 'result' => $result];
+        } else if ($result->transaction) {
+            return ['status' => false, 'result' => $result];
+        } else {
+            return ['status' => false, 'result' => $result];
+        }
+    }
+
+    public function saveCustomer()
+    {
+        $result = \Braintree_Customer::create($this->options['customer']);
+
+        if ($result->success) {
+            return ['status' => true, 'result' => $result];
+        } else {
+            return ['status' => false, 'result' => $result];
+        }
+    }
+
+    public function saveCreditCard()
+    {
+        $send_array = $this->options['creditCard'];
+        if (isset($this->options['billing'])) {
+            $send_array['billingAddress'] = $this->options['billing'];
+        }
+        if (isset($this->options['customerId'])) {
+            $send_array['customerId'] = $this->options['customerId'];
+        }
+        $result = \Braintree_CreditCard::create($send_array);
+
+        if ($result->success) {
+            return ['status' => true, 'result' => $result];
+        } else {
+            return ['status' => false, 'result' => $result];
+        }
+    }
+
+    public function saveAddress()
+    {
+        $send_array = $this->options['billing'];
+        if (isset($this->options['customerId'])) {
+            $send_array['customerId'] = $this->options['customerId'];
+        }
+        $result = \Braintree_Address::create($send_array);
+
+        if ($result->success) {
+            return ['status' => true, 'result' => $result];
+        } else {
+            return ['status' => false, 'result' => $result];
+        }
+    }
+
+    /**
+     * Constructs the Credit Card array for payment
+     * @param integer $number Credit Card Number
+     * @param integer $cvv (optional)Credit Card Security code
+     * @param integer $expirationMonth format: MM (use expirationMonth and expirationYear or expirationDate not both)
+     * @param integer $expirationYear format: YYYY (use expirationMonth and expirationYear or expirationDate not both)
+     * @param string $expirationDate format: MM/YYYY (use expirationMonth and expirationYear or expirationDate not both)
+     */
+    public function setCreditCard($number, $cvv = null, $expirationMonth = null, $expirationYear = null, $expirationDate = null)
+    {
+        $this->options['creditCard'] = [];
+        $this->options['creditCard']['number'] = $number;
+        if (isset($cvv)) $this->options['creditCard']['cvv'] = $cvv;
+        if (isset($expirationMonth)) $this->options['creditCard']['expirationMonth'] = $expirationMonth;
+        if (isset($expirationYear)) $this->options['creditCard']['expirationYear'] = $expirationYear;
+        if (isset($expirationDate)) $this->options['creditCard']['expirationDate'] = $expirationDate;
+    }
+
+    public function getCreditCard($input_values)
+    {
+        $default = [
+            'cvv' => null,
+            'month' => null,
+            'year' => null,
+            'date' => null,
+            'name' => null,
+        ];
+        $values = array_merge($default, $input_values);
+        $this->setCreditCard($values['number'], $values['cvv'], $values['month'], $values['year'], $values['date'], $values['name']);
+    }
+
+    public function getOptions($values)
+    {
+        if (isset($values['amount'])) $this->setAmount($values['amount']);
+        if (isset($values['orderId'])) $this->options['orderId'] = $values['orderId'];
+        if (isset($values['creditCard'])) $this->getCreditCard($values['creditCard']);
+        if (isset($values['customer'])) $this->options['customer'] = $values['customer'];
+        if (isset($values['billing'])) $this->options['billing'] = $values['billing'];
+        if (isset($values['shipping'])) $this->options['shipping'] = $values['shipping'];
+
+        //For storing payment method in vault
+        if (isset($values['customerId'])) $this->options['customerId'] = $values['customerId'];
+    }
+
+    /**
+     * Set the amount to charge
+     * @param float $amount No dollar sign needed
+     */
+    public function setAmount($amount)
+    {
+        $this->options['amount'] = round($amount, 2);
+    }
+}
--- BraintreeForm.php
+++ BraintreeForm.php
@@ -1,7 +1,190 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: anton
- * Date: 27.05.14
- * Time: 16:53
- */ 
+namespace yii\braintree;
+
+use yii\base\Model;
+use yii\helpers\Html;
+
+class BraintreeForm extends Model
+{
+
+    public $amount;
+    public $orderId;
+
+    public $creditCard_number;
+    public $creditCard_cvv;
+    public $creditCard_month;
+    public $creditCard_year;
+    public $creditCard_date;
+    public $creditCard_name;
+
+    public $customer_firstName;
+    public $customer_lastName;
+    public $customer_company;
+    public $customer_phone;
+    public $customer_fax;
+    public $customer_website;
+    public $customer_email;
+
+    public $billing_firstName;
+    public $billing_lastName;
+    public $billing_company;
+    public $billing_streetAddress;
+    public $billing_extendedAddress;
+    public $billing_locality;
+    public $billing_region;
+    public $billing_postalCode;
+    public $billing_countryCodeAlpha2;
+
+    public $shipping_firstName;
+    public $shipping_lastName;
+    public $shipping_company;
+    public $shipping_streetAddress;
+    public $shipping_extendedAddress;
+    public $shipping_locality;
+    public $shipping_region;
+    public $shipping_postalCode;
+    public $shipping_countryCodeAlpha2;
+
+    public $customerId;
+
+    public function rules()
+    {
+        return [
+            [['customerId'], 'required', 'on' => 'creditCard'],
+            [['customerId'], 'required', 'on' => 'address'],
+            [['customer_firstName', 'customer_lastName'], 'required', 'on' => 'customer'],
+            [['amount'], 'required', 'on' => 'charge'],
+            [['amount'], 'numerical'],
+            [['customer_email'], 'email'],
+            [['creditCard_number', ' creditCard_cvv', ' creditCard_month', ' creditCard_year', ' creditCard_date',
+                'customer_firstName',
+                'customer_lastName',
+                'customer_company',
+                'customer_phone',
+                'customer_fax',
+                'customer_website',
+                'billing_firstName',
+                'billing_lastName',
+                'billing_company',
+                'billing_streetAddress',
+                'billing_extendedAddress',
+                'billing_locality',
+                'billing_region',
+                'billing_postalCode',
+                'billing_countryCodeAlpha2',
+                'shipping_firstName',
+                'shipping_lastName',
+                'shipping_company',
+                'shipping_streetAddress',
+                'shipping_extendedAddress',
+                'shipping_locality',
+                'shipping_region',
+                'shipping_postalCode',
+                'shipping_countryCodeAlpha2'], 'safe'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'amount' => 'Amount($)',
+            'orderId' => 'Order ID',
+            'creditCard_number' => 'Credit Card Number',
+            'creditCard_cvv' => 'Security Code',
+            'creditCard_month' => 'Expiration Month (MM)',
+            'creditCard_year' => 'Expiration Year (YYYY)',
+            'creditCard_date' => 'Expiration Date (MM/YYYY)',
+            'creditCard_name' => 'Name on Card',
+            'customer_firstName' => 'First Name',
+            'customer_lastName' => 'Last Name',
+            'customer_company' => 'Company Name',
+            'customer_phone' => 'Phone Number',
+            'customer_fax' => 'Fax Number',
+            'customer_website' => 'Website',
+            'customer_email' => 'Email',
+            'billing_firstName' => 'First Name',
+            'billing_lastName' => 'Last Name',
+            'billing_company' => 'Company Name',
+            'billing_streetAddress' => 'Address',
+            'billing_extendedAddress' => 'Address',
+            'billing_locality' => 'City/Locality',
+            'billing_region' => 'State/Region',
+            'billing_postalCode' => 'Zip/Postal Code',
+            'billing_countryCodeAlpha2' => 'Country',
+            'shipping_firstName' => 'First Name',
+            'shipping_lastName' => 'Last Name',
+            'shipping_company' => 'Company Name',
+            'shipping_streetAddress' => 'Address',
+            'shipping_extendedAddress' => 'Address',
+            'shipping_locality' => 'City/Locality',
+            'shipping_region' => 'State/Region',
+            'shipping_postalCode' => 'Zip/Postal Code',
+            'shipping_countryCodeAlpha2' => 'Country',
+        ];
+    }
+
+    public function send()
+    {
+        $values = array();
+        foreach ($this->attributes as $key => $val) {
+            if (!is_object($val) && !is_null($val) && strlen($val) > 0) {
+                if (strpos($key, '_') === false) {
+                    $values[$key] = $val;
+                } else {
+                    $pieces = explode('_', $key);
+                    $values[$pieces[0]][$pieces[1]] = $val;
+                }
+            }
+        }
+        \Yii::$app->get('braintree')->getOptions($values);
+        $scenario = $this->getScenario();
+        return $this->$scenario();
+
+    }
+
+    public function charge()
+    {
+        $return = \Yii::$app->get('braintree')->singleCharge();
+        if ($return['status'] === false) {
+            $this->addError('creditCard_number', $return['result']->_attributes['message']);
+            return false;
+        } else {
+            return $return;
+        }
+    }
+
+    public function customer()
+    {
+        $return = \Yii::$app->get('braintree')->saveCustomer();
+        if ($return['status'] === false) {
+            foreach (($return['result']->errors->deepAll()) as $error) {
+                $this->addError('customer_firstName', $error->message);
+            }
+            return false;
+        } else {
+            return $return;
+        }
+    }
+
+    public function creditCard()
+    {
+        $return = \Yii::$app->get('braintree')->saveCreditCard();
+        if ($return['status'] === false) {
+            $this->addError('creditCard_number', 'Error saving card');
+            return false;
+        } else {
+            return $return;
+        }
+    }
+
+    public function address()
+    {
+        $return = \Yii::$app->get('braintree')->saveAddress();
+        if ($return['status'] === false) {
+            $this->addError('billing_firstName', 'Error saving card');
+            return false;
+        } else {
+            return $return;
+        }
+    }
+}
